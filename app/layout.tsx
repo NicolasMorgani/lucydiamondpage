9a3785344a965e7dd00f2b086654168c2b31bcf4
Nico Morgani <nico_morgani@hotmail.com>
2024-08-31 19:31:13 -0300

    footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 
 export const metadata: Metadata = {
-  title: "Jose de San Martin",
+  title: "Jose de San Martin 👀",
   description: "Created by Nico Morgani",
 };
 
@@ -25,7 +25,7 @@ export default function RootLayout({
       <head>
         
       <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"></link>
-      <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+      <script src="https://unpkg.com/aos@2.3.1/dist/aos.js" async></script>
       </head>
 
       <body className={inter.className}>
@@ -40,6 +40,15 @@ export default function RootLayout({
 
             {children}
           </main>
+          <footer >
+            <div className="flex justify-center items-center mt-40 " >
+                  <p>NicoMorgani ❤</p>
+            </div>
+            <div className="flex justify-center items-center mb-28" >
+                  <p>Todos los Derechos Reservados 😎</p>
+            </div>
+            
+          </footer>
         </ThemeProvider>
         <script>
   AOS.init();
